5df2aaa16e5fe07387f970249c3c08f273d8ceef
David Yaari <david,yaari@icloud.com>
2023-11-29 16:26:53 +0200

    responsive for iPhone

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -10,28 +10,28 @@ const Items: React.FC<Props> = ({ coin }) => {
     <>
       <div className='container text-center'>
         <div className='row'>
-          <div className='col'>
+          <div className='col-sm'>
             <p>{coin?.market_cap_rank}</p>
           </div>
-          <div className='col'>
+          <div className='col-sm'>
             <img src={coin?.image} alt={coin?.name} width='40' height='40' />
           </div>
-          <div className='col'>
+          <div className='col-sm'>
             <p>{coin?.symbol.toUpperCase()}</p>
           </div>
-          <div className='col'>
+          <div className='col-sm'>
             <p>${coin?.current_price.toLocaleString()}</p>
           </div>
-          <div className='col'>
+          <div className='col-sm'>
             <p>${coin?.current_price.toLocaleString()}</p>
           </div>
-          <div className='col'>
+          <div className='col-sm'>
             <p>${coin?.current_price.toLocaleString()}</p>
           </div>
-          <div className='col'>
+          <div className='col-sm'>
             <p>${coin?.total_volume.toLocaleString()}</p>
           </div>
-          <div className='col'>
+          <div className='col-sm'>
             <p>${coin?.market_cap.toLocaleString()}</p>
           </div>
         </div>
